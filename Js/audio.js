744f92d4ca808ae9728907405179aebdeab2b3db
MiiiL0VE <mikokun9092@gmail.com>
2020-05-03 21:16:38 +0700

    MiiiL0VE V.1

---
+++ Js/audio.js
@@ -0,0 +1,18 @@
+const button = document.querySelector("#button");
+const icon = document.querySelector("#button > i");
+const audio = document.querySelector("audio");
+
+button.addEventListener("click", () => {
+  if (audio.paused) {
+    audio.volume = 0.2;
+    audio.play();
+    icon.classList.remove('fa-volume-up');
+    icon.classList.add('fa-volume-mute');
+    
+  } else {
+    audio.pause();
+    icon.classList.remove('fa-volume-mute');
+    icon.classList.add('fa-volume-up');
+  }
+  button.classList.add("fade");
+});
---
+++ Js/typer.js
@@ -0,0 +1,35 @@
+var line = document.getElementById("line")
+var txts = ["Whatever happens, keep breathing", "I may not with you everyday, but I love you everyday", "I love you", "I'm sorry for loving you"];
+var speed = 100
+
+async function typewriter(txt) {
+  for(let i = 0; i < txt.length; i++){
+    line.innerHTML += txt.charAt(i);
+    await delay(speed) 
+  }
+}
+
+async function reverseTypewriter(txt) {
+  for(let i = txt.length; i > 0; i--){
+    line.innerHTML = line.innerHTML.slice(0,-1)
+    await delay(speed)
+  }
+}
+
+async function writeLoop(){
+  
+  for(let i = 0; i < txts.length; i++){
+    await typewriter(txts[i])
+    await delay(4000)
+    await reverseTypewriter(txts[i])
+    await delay(1000)
+  }
+  
+  writeLoop()
+}
+
+function delay(ms){
+  return new Promise((resolve)=>{setTimeout(()=>{resolve()},ms)})
+}
+
+writeLoop()
